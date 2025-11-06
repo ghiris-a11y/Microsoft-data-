e98df102611cd0f67c1c54fc6758aa1ce8ed5ac6
ghiris-a11y <2025.ghiris@isu.ac.in>
2025-11-06 18:47:27 +0530

    Add files via upload

---
+++ msft_app.py
@@ -0,0 +1,103 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[2]:
+
+
+pip install --upgrade numpy pandas matplotlib scipy --user
+
+
+# In[2]:
+
+
+import streamlit as st
+import pandas as pd
+import matplotlib.pyplot as plt
+
+
+# In[3]:
+
+
+
+income = pd.read_csv("msft_income.csv", index_col=0)
+bs = pd.read_csv("msft_balance_sheet.csv", index_col=0)
+cf = pd.read_csv("msft_cashflow.csv", index_col=0)
+ratios = pd.read_csv("msft_ratios(1).csv", index_col=0)
+
+
+# In[6]:
+
+
+pip install streamlit-jupyter
+
+
+# In[7]:
+
+
+
+from streamlit_jupyter import StreamlitPatcher
+StreamlitPatcher().jupyter()
+
+
+# In[4]:
+
+
+
+st.sidebar.title("Navigation")
+section = st.sidebar.radio("Go to", ["Overview", "Ratios", "Valuation"])
+
+
+# In[5]:
+
+
+
+if section == "Overview":
+    st.title("Microsoft Financial Overview")
+    st.write("### Income Statement")
+    st.dataframe(income.tail())
+    st.write("### Balance Sheet")
+    st.dataframe(bs.tail())
+    st.write("### Cash Flow")
+    st.dataframe(cf.tail())
+
+
+# In[7]:
+
+
+
+if section == "Ratios":
+    st.title("Financial Ratios")
+    st.dataframe(ratios)
+
+    st.write("### Ratio Trends")
+    selected_ratio = st.selectbox("Select a ratio to plot", ratios.columns)
+    st.line_chart(ratios[selected_ratio])
+
+
+# In[9]:
+
+
+if section == "Valuation":
+    st.title("DCF Valuation")
+
+    last_fcf = cf["Operating Activities"].iloc[-1] - cf["Capital Expenditures"].iloc[-1]
+    discount_rate = 0.08
+    growth_rate = 0.05
+    years = 5
+
+    projected_fcfs = [last_fcf * ((1 + growth_rate) ** i) for i in range(1, years + 1)]
+    discounted_fcfs = [fcf / ((1 + discount_rate) ** i) for i, fcf in enumerate(projected_fcfs, 1)]
+
+    terminal_value = projected_fcfs[-1] * (1 + growth_rate) / (discount_rate - growth_rate)
+    discounted_terminal = terminal_value / ((1 + discount_rate) ** years)
+
+    dcf_value = sum(discounted_fcfs) + discounted_terminal
+
+    st.metric(label="Estimated DCF Value", value=f"${dcf_value:,.2f}")
+
+
+# In[ ]:
+
+
+
+
